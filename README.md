d2ef278f3203bd937a47ba1577dfe21dad87a59a
Samuel Hammill <shammill88@gmail.com>
2024-04-01 14:58:59 +1000

    feat: simulation base. signalr hub and a background service to run the sim.

---
+++ README.md
@@ -0,0 +1,10 @@
+**A real-time simulation with a SignalR interface**
+
+Current Functionality: 
+* Simulation framework
+
+Future Development:
+* Simulation components (movement, activities, etc)
+* SignalR Interface
+* SignalR Client
+
---
+++ Shammill.Simulation/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+
+namespace Shammill.Simulation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddSignalR();
+            builder.Services.AddHostedService<SimulationRunner>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            //app.UseHttpsRedirection();
+            //app.UseStaticFiles();
+            app.UseRouting();
+
+            // Map SignalR Hub
+            app.MapHub<UpdateHub>("/chatHub");
+
+            app.Run();
+        }
+    }
+}
+
